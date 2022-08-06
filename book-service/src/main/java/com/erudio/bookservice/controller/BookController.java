41a5ef721d8efbe8ea5a6bcd77670c0a1de13e73
Felipe <felipecostazucco@gmail.com>
2022-08-06 20:20:17 -0300

    api

--- book-service/src/main/java/com/erudio/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/erudio/bookservice/controller/BookController.java
@@ -4,6 +4,8 @@ import com.erudio.bookservice.model.Book;
 import com.erudio.bookservice.proxy.CambioProxy;
 import com.erudio.bookservice.repository.BookRepository;
 import com.erudio.bookservice.response.CambioResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Optional;
 
+@Tag(name = "Book Endpoint")
 @RestController
 @RequestMapping("/book-service")
 public class BookController {
@@ -30,22 +33,20 @@ public class BookController {
     @Autowired
     private CambioProxy proxy;
 
+    @Operation(summary = "Find a specific book by its ID")
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
         String port = environment.getProperty("local.server.port");
         Optional<Book> optBook = repository.findById(id);
         if (optBook.isPresent()) {
             Book book = optBook.get();
-            book.setEnvironment(port);
 
             CambioResponse cambio = proxy.getCambio(book.getPrice(), "USD", currency);
+            book.setEnvironment("Book port: " + port + ", Cambio port: " + cambio.getEnvironment());
 
-            assert cambio != null;
             book.setPrice(cambio.getConvertedValue());
             return book;
         }
-
-
 
         throw new RuntimeException("Book no exist.");
     }
--- cambio-service/src/main/java/com/erudio/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/erudio/cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package com.erudio.cambioservice.controller;
 
 import com.erudio.cambioservice.model.Cambio;
 import com.erudio.cambioservice.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Tag(name = "Cambio Endpoint")
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
@@ -22,6 +25,7 @@ public class CambioController {
     @Autowired
     private CambioRepository repository;
 
+    @Operation(summary = "Find cambio through amount/from/to")
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
                             @PathVariable("from") String from,
